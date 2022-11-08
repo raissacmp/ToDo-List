bd40672ae7566ef985b0533dbf6dc762fd28f8e9
raissa <raissa.campos26@gmail.com>
2022-11-08 15:41:21 -0300

    feat: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { Header } from "./components/Header";
 
-import styles from "./App.module.css";
-console.log("🚀 ~ file: App.tsx ~ line 5 ~ styles", styles);
+// import styles from "./App.module.css";
 
 import "./global.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a className={styles.logo} href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  );
+  return <Header />;
 }
 
 export default App;
