9b580db144ae075b39118ff8711c8d052ec33eb0
raissa <raissa.campos26@gmail.com>
2022-11-08 17:08:31 -0300

    feat: initial tasks

---
+++ src/components/Tasks.tsx
@@ -0,0 +1,17 @@
+import styles from "./Taks.module.css";
+
+export function Tasks() {
+  return (
+    <div>
+      <header>
+        <strong>
+          Tarefas criadas <span>5</span>
+        </strong>
+        <strong>
+          Concluidas <span>2 de 5</span>
+        </strong>
+      </header>
+      <p></p>
+    </div>
+  );
+}
