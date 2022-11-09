25f9cbd995c127a22cb7d4e92876e6359d36b674
raissa <raissa.campos26@gmail.com>
2022-11-09 10:29:29 -0300

    styles: taks content

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,17 +1,41 @@
-import styles from "./Taks.module.css";
+import styles from "./Tasks.module.css";
+import { Trash } from "phosphor-react";
 
 export function Tasks() {
   return (
-    <div>
-      <header>
-        <strong>
+    <div className={styles.containerTasks}>
+      <header className={styles.headerTasks}>
+        <strong className={styles.allTasks}>
           Tarefas criadas <span>5</span>
         </strong>
-        <strong>
+        <strong className={styles.progressTasks}>
           Concluidas <span>2 de 5</span>
         </strong>
       </header>
-      <p></p>
+      <div className={styles.contentTask}>
+        <input type="checkbox" />
+        <p className={styles.textTask}>
+          Integer urna interdum massa libero auctor neque turpis turpis semper.
+          Duis vel sed fames integer.
+        </p>
+        <Trash color="gray" size={20} />
+      </div>
+      <div className={styles.contentTask}>
+        <input type="checkbox" />
+        <p className={styles.textTask}>
+          Integer urna interdum massa libero auctor neque turpis turpis semper.
+          Duis vel sed fames integer.
+        </p>
+        <Trash color="gray" size={20} />
+      </div>
+      <div className={styles.contentTask}>
+        <input type="checkbox" />
+        <p className={styles.textTask}>
+          Integer urna interdum massa libero auctor neque turpis turpis semper.
+          Duis vel sed fames integer.
+        </p>
+        <Trash color="gray" size={20} />
+      </div>
     </div>
   );
 }
